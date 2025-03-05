ad1c16538d28af403989ac04180831cb94f0a2be
Peter Muhumuki <muhumukip@gmail.com>
2025-03-05 06:57:26 +0300

    Update PostAdmin list display order and add tests for Post model

--- posts/admin.py
+++ posts/admin.py
@@ -4,6 +4,7 @@ from .models import Post
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['author', 'title']
+    list_display = ['title', 'author']
     list_filter = ['author']
     search_fields = ['author__username', 'title']
+    
